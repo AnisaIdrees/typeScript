29e330015a9074efc165573a4e64b58baf3b99da
AnisaIdrees <AnisaIdrees191@gmail.com>
2025-03-24 06:26:35 +0500

    type annotation/type inference / implicitly /explicitly

--- typScript-practice/dist/index.js
+++ typScript-practice/dist/index.js
@@ -1,3 +1,17 @@
 "use strict";
-let num = 20;
+let num = 20; // type annotaion 
 console.log(num);
+num = 23;
+console.log(num);
+let Fname = 'Aneesa'; //type infernece 
+console.log(Fname);
+Fname = 'Aneesa Idrees';
+console.log(Fname);
+let x;
+x = 50;
+console.log(x); //  any implicitly
+x = '40';
+console.log(x); //  any implicitly
+let a = 100;
+// a='aneesa'
+console.log(a); // type inference 
--- typScript-practice/src/index.ts
+++ typScript-practice/src/index.ts
@@ -1,2 +1,22 @@
-let num :number = 20;
+let num :number = 20;// type annotaion 
 console.log(num);
+num=23
+console.log(num);
+
+
+let Fname ='Aneesa'; //type infernece 
+console.log(Fname);
+Fname='Aneesa Idrees';
+console.log(Fname);
+
+
+let x ;
+x= 50;
+console.log(x);//  any implicitly
+x='40'
+console.log(x);//  any implicitly
+
+
+let a =100;
+// a='aneesa'
+console.log(a); // type inference 
