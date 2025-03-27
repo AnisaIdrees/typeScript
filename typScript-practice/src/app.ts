d7c21dcadcba7759fbb60740b3b8f04f3e9bd8ce
AnisaIdrees <AnisaIdrees191@gmail.com>
2025-03-27 21:55:31 +0500

    Type Aliases and Interfaces

--- typScript-practice/src/app.ts
+++ typScript-practice/src/app.ts
@@ -46,3 +46,46 @@ class Person{
 const graph: [x: number, y: number] = [55.2, 41.3];
 // const graph: [number, number] = [55.2, 41.3];
 // const [x, y] = graph;
+
+
+
+/*TypeScript allows types to be defined separately from the variables that use them.
+
+Aliases and Interfaces allows types to be easily shared between different variables/objects.
+
+Type Aliases
+Type Aliases allow defining types with a custom name (an Alias).
+
+Type Aliases can be used for primitives like string or more complex types such as objects and arrays:
+
+ExampleGet your own TypeScript Server*/
+type CarYear = number
+type CarType = string
+type CarModel = string
+type Car = {
+  year: CarYear,
+  type: CarType,
+  model: CarModel
+}
+
+const carYear: CarYear = 2001
+const carType: CarType = "Toyota"
+const carModel: CarModel = "Corolla"
+const car: Car = {
+  year: carYear,
+  type: carType,
+  model: carModel
+};
+/*Interfaces
+Interfaces are similar to type aliases, except they only apply to object types.*/
+
+//Example
+interface Rectangle {
+  height: number,
+  width: number
+}
+
+const rectangle: Rectangle = {
+  height: 20,
+  width: 10
+};
